2dc7b7ca9828badcc95eba68e0af983f74514141
David Pilato <david@pilato.fr>
2012-10-09 16:14:04 +0200

    Wondering why properties are not injected

--- src/main/java/fr/pilato/maven/plugins/ESStartMojo.java
+++ src/main/java/fr/pilato/maven/plugins/ESStartMojo.java
@@ -1,9 +1,16 @@
 package fr.pilato.maven.plugins;
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.descriptor.PluginDescriptor;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.settings.Settings;
 
 /**
  * Start an elasticsearch node when starting integration tests
@@ -12,11 +19,48 @@ import org.apache.maven.plugins.annotations.Mojo;
 public class ESStartMojo
     extends AbstractMojo
 {
+    @Component
+    private MavenSession session;
+
+    @Component
+    private MavenProject project;
+
+    @Component
+    private MojoExecution mojo;
+
+    @Component
+    private PluginDescriptor plugin;
+
+    @Component
+    private Settings settings;	
+
+    @Parameter(property="thebar", 
+                required=true, 
+                defaultValue="coolbar" )
+    protected String bar;	
+	
+    @Parameter(property="esdir", 
+            required=true, 
+            defaultValue="coolbar" )
+    protected String esdir;	
+
     public void execute()
         throws MojoExecutionException
     {
     	
-		getLog().error("STARTING MOJO");
+		getLog().error("STARTING MOJO " + bar + " " + esdir);
+		
+		// Node node = NodeBuilder.nodeBuilder().node();
 
     }
+    
+    public void setBar(String bar) {
+		this.bar = bar;
+	}
+    
+    public String getBar() {
+		return bar;
+	}
+    
+    
 }
